0c059043728385a4d5a0e7034f713c62264ab9a3
Fernando Chavez <fjchavez01@gmail.com>
2020-11-03 12:51:02 -0800

    got redux to work

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import 'bulma/css/bulma.css';
 import './styles.scss';
+import { createStore } from 'redux'
+import { theReducer } from './reducer'
+import { Provider } from 'react-redux'
+const store = createStore(theReducer)
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store} ><App /></Provider>, rootElement);
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -15,7 +15,7 @@ const initialState = {
     ]
 }
 
-export const theReducer = (state=initialState, action){
+export const theReducer = (state=initialState, action) => {
     switch(action.type){
         default:
             return state
